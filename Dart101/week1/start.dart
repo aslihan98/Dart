bac149f5091bd287c59d4a303205fa8f434ce40a
aslihan98 <aslihanevcil@outlook.com>
2022-05-28 16:03:41 +0300

    Print-Variables-Data Types

---
+++ Dart101/week1/start.dart
@@ -0,0 +1,18 @@
+void main() {
+  print('Hello Dart!');
+  print('hello'+'guest');
+  print(2+6);
+
+  String message = 'Hello guest!';
+  print(message);
+
+  //command line
+
+  int money= 15;
+  print(money);
+
+  print(message + money.toString());
+
+
+
+}
---
+++ Dart101/week1/user_basic.dart
@@ -0,0 +1,14 @@
+void main() {
+  String customerName= 'KLM Company';
+  String message= 'Welcome';
+  double customerMoney= 456.7;
+
+
+  print(message + customerName + '!'
+  + 'Your Balance = '+customerMoney.toString());
+
+
+
+
+
+}
