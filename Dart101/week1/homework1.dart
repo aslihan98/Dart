7b6db911185ef76e14665af769507538f90cf0e2
aslihan98 <aslihanevcil@outlook.com>
2022-05-29 15:40:46 +0300

    Homework1

---
+++ Dart101/week1/homework1.dart
@@ -0,0 +1,16 @@
+void main() {
+  /*Create a new user.
+  ,Set a new money amount.
+  Divide the money by 20. Print it on the screen.
+  */
+  String user ='Bas Company';
+  int userMoney=786532;
+  userMoney = userMoney~/20;
+  print('Your balance:'+' '+userMoney.toString());
+
+
+
+}
+
+
+
